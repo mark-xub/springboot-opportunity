2a7f2a820d44c8b3ae36e05aed34eb36ed9199f6
mark.xub <mark.xub@alibaba-inc.com>
2017-06-28 14:52:31 +0800

    第一次提交

---
+++ src/main/java/com/mark/opportunitycenter/OpportunityCenterApplication.java
@@ -0,0 +1,12 @@
+package com.mark.opportunitycenter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpportunityCenterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpportunityCenterApplication.class, args);
+	}
+}
